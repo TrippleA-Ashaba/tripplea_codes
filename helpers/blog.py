e25232f6cc7bc15bc0278a7ec714bd69ffdf4ac6
TrippleA-Ashaba <trippleaunit@hotmail.com.com>
2023-05-26 14:09:31 +0300

    Add blogs

--- helpers/blog.py
+++ helpers/blog.py
@@ -1,9 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ.get("BLOG_API_KEY")
 
 
 def get_blogs():
     r = requests.get(
-        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@albertashaba.a"
+        f"https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@albertashaba.a&api_key={API_KEY}"
     )
     data = r.json()
     blogs = data["items"]
